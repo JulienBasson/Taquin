02e4a61de099061117fd8939162f673225b92a4e
Elisa Lescarret <lescarre@ensipc60.imag.fr>
2016-03-03 16:53:54 +0100

    lecture fichier buguee

--- Main.java
+++ Main.java
@@ -2,27 +2,48 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.Random;
+import java.io.FileNotFoundException;
 
 public class Main {
-    private static Direction randomDirection(Set<Direction> set){
-        Random rand = new Random(System.currentTimeMillis());
-        List<Direction> setArray = new ArrayList<Direction>(set);
-        return setArray.get(rand.nextInt(set.size()));
-    }
-    public static void main(String[] args) {
-        State orig = new State(5);
-        System.out.println(orig);
-        State state = new State(orig);
-        Algorithm algo = new IterativeDeepeningAStar(orig);
+	private static Direction randomDirection(Set<Direction> set) {
+		Random rand = new Random(System.currentTimeMillis());
+		List<Direction> setArray = new ArrayList<Direction>(set);
+		return setArray.get(rand.nextInt(set.size()));
+	}
 
-        List<Direction> moves = new ArrayList<Direction>();
-        for (int i = 0; i < 20; i++) {
-            Direction nextMove = randomDirection(state.availableMoves());
-            moves.add(nextMove);
-            state = state.move(nextMove);
-        }
-        System.out.println(state);
-        System.out.println(moves);
-        System.out.println(algo.solve(state));
-    }
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			try {
+				testingFile(args[0]);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			State orig = new State(5);
+			System.out.println(orig);
+			State state = new State(orig);
+			Algorithm algo = new IterativeDeepeningAStar(orig);
+
+			List<Direction> moves = new ArrayList<Direction>();
+			for (int i = 0; i < 20; i++) {
+				Direction nextMove = randomDirection(state.availableMoves());
+				moves.add(nextMove);
+				state = state.move(nextMove);
+			}
+			System.out.println(state);
+			System.out.println(moves);
+			System.out.println(algo.solve(state));
+		}
+	}
+
+	private static void testingFile(String file) throws FileNotFoundException {
+		ReadFile rf = new ReadFile(file);
+		ReadFile.Init contents = rf.getDataFile();
+		State original = new State(contents.size, contents.target);
+		Algorithm algo = new IterativeDeepeningAStar(original);
+		State state = new State(contents.size, contents.init);
+
+		System.out.println(algo.solve(state));
+
+	}
 }
---
+++ ReadFile.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ReadFile {
+	private BufferedReader br;
+	private InputStreamReader ipsr;
+	private InputStream ips;
+	
+	public ReadFile(String fichier) throws FileNotFoundException
+	{
+		ips = new FileInputStream(fichier);
+		ipsr = new InputStreamReader(ips);
+		br = new BufferedReader(ipsr);
+	}
+	
+	public Init getDataFile() {
+		List<Integer> init = new ArrayList<Integer>();
+		List<Integer> target = new ArrayList<Integer>();
+		int nbOfLine = 0;
+		
+		// lecture du fichier texte
+		try {
+			
+			String ligne = br.readLine();
+			nbOfLine = Integer.parseInt(ligne.trim());
+			int nbLineActu = 0;
+
+			while ((ligne = br.readLine()) != null && nbLineActu < nbOfLine) {
+				List<String> chars = new ArrayList<String>(Arrays.asList(ligne.split("\\s+")));
+				for(String c : chars){
+					init.add(Integer.parseInt(c));
+				}
+				nbLineActu++;
+			}
+			
+			while ((ligne = br.readLine()) != null)
+			{
+				List<String> chars = new ArrayList<String>(Arrays.asList(ligne.split("\\s+")));
+				for(String c : chars){
+					target.add(Integer.parseInt(c));
+				}
+				nbLineActu++;			}
+			
+			
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		return new ReadFile.Init(nbOfLine, init, target);
+	}
+	
+	public class Init
+	{
+		public final int size;
+		public final List<Integer> init;
+		public final List<Integer> target;
+		
+		public Init (int s, List<Integer> i, List<Integer> t)
+		{
+			this.size = s;
+			this.init = i;
+			this.target = t;
+		}
+	}
+	
+	
+	
+}
