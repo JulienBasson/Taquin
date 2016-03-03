974d5a6942e04a80f00fd9e3bad9f1722ccf52a8
Elisa Lescarret <lescarre@ensipc60.imag.fr>
2016-03-03 17:09:01 +0100

    lecture fichier reparer

--- ReadFile.java
+++ ReadFile.java
@@ -12,43 +12,42 @@ public class ReadFile {
 	private BufferedReader br;
 	private InputStreamReader ipsr;
 	private InputStream ips;
-	
-	public ReadFile(String fichier) throws FileNotFoundException
-	{
+
+	public ReadFile(String fichier) throws FileNotFoundException {
 		ips = new FileInputStream(fichier);
 		ipsr = new InputStreamReader(ips);
 		br = new BufferedReader(ipsr);
 	}
-	
+
 	public Init getDataFile() {
 		List<Integer> init = new ArrayList<Integer>();
 		List<Integer> target = new ArrayList<Integer>();
 		int nbOfLine = 0;
-		
+
 		// lecture du fichier texte
 		try {
-			
+
 			String ligne = br.readLine();
 			nbOfLine = Integer.parseInt(ligne.trim());
 			int nbLineActu = 0;
 
-			while ((ligne = br.readLine()) != null && nbLineActu < nbOfLine) {
+			while ( nbLineActu < nbOfLine && (ligne = br.readLine()) != null) {
 				List<String> chars = new ArrayList<String>(Arrays.asList(ligne.split("\\s+")));
-				for(String c : chars){
+				for (String c : chars) {
 					init.add(Integer.parseInt(c));
 				}
 				nbLineActu++;
 			}
 			
-			while ((ligne = br.readLine()) != null)
-			{
+			nbLineActu = 0;
+			while (nbLineActu < nbOfLine && (ligne = br.readLine()) != null) {
 				List<String> chars = new ArrayList<String>(Arrays.asList(ligne.split("\\s+")));
-				for(String c : chars){
+				for (String c : chars) {
 					target.add(Integer.parseInt(c));
 				}
-				nbLineActu++;			}
-			
-			
+				nbLineActu++;
+			}
+
 			br.close();
 		} catch (Exception e) {
 			System.out.println(e.toString());
@@ -56,21 +55,17 @@ public class ReadFile {
 
 		return new ReadFile.Init(nbOfLine, init, target);
 	}
-	
-	public class Init
-	{
+
+	public class Init {
 		public final int size;
 		public final List<Integer> init;
 		public final List<Integer> target;
-		
-		public Init (int s, List<Integer> i, List<Integer> t)
-		{
+
+		public Init(int s, List<Integer> i, List<Integer> t) {
 			this.size = s;
 			this.init = i;
 			this.target = t;
 		}
 	}
-	
-	
-	
+
 }
